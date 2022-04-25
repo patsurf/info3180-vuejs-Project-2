115ee34474c0acaa5fae0291bb2fdf079de976f4
Jon-Daniel Coombs <71227832+Coder-Jon014@users.noreply.github.com>
2022-04-24 20:38:33 -0500

    Edits

---
+++ migrations/versions/876a452c7a5b_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: 876a452c7a5b
+Revises: 742b25e79206, 94980dd159e6
+Create Date: 2022-04-24 20:36:20.747491
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '876a452c7a5b'
+down_revision = ('742b25e79206', '94980dd159e6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
