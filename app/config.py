55cc6e177ef34f99aacc577c48a8f03648834215
Jon-Daniel Coombs <71227832+Coder-Jon014@users.noreply.github.com>
2022-05-02 15:25:24 -0500

    Edit

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()  # load environment variables from .env if it exists.
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    UPLOAD_FOLDER = 'uploads'
+    UPLOAD_FOLDER = './uploads'
     SECRET_KEY = 'r@ndomK3y'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed'
